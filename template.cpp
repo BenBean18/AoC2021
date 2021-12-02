999b8d46ab6b36eef6c162d687345c5b4a0b5e03
BenBean18 <beakerthespeaker@gmail.com>
2021-12-01 21:18:00 -0500

    Added snippets to make Day 2+ easier

--- template.cpp
+++ template.cpp
@@ -2,21 +2,44 @@
 #include <fstream>
 #include <vector>
 
-int main(int argc, char** argv) {
-    std::vector<std::string> lines;
+std::vector<std::string> getStrings() {
+    std::vector<std::string> strings;
     std::string line;
 
     std::ifstream input_file("day1.txt");
     if (!input_file.is_open()) {
         std::cerr << "Error opening file" << std::endl;
-        return EXIT_FAILURE;
+        return {};
     }
 
     while (getline(input_file, line)) {
-        lines.push_back(line);
-        // or do something else
+        strings.push_back(line);
     }
 
     input_file.close();
+    return strings;
+}
+
+std::vector<int> getInts() {
+    std::vector<int> ints;
+    std::string line;
+
+    std::ifstream input_file("day1.txt");
+    if (!input_file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        return {};
+    }
+
+    while (getline(input_file, line)) {
+        ints.push_back(std::stoi(line));
+    }
+
+    input_file.close();
+    return ints;
+}
+
+int main(int argc, char** argv) {
+    // Insert code here! Use snippets -- "strs" = strings, "ints" = ints, "oth" = other
+    
     return 0;
 }
