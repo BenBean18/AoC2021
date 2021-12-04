fc1587138b6f1472c2e58cff5693d24909b0573f
BenBean18 <beakerthespeaker@gmail.com>
2021-12-04 11:32:00 -0500

    Diagonals don't count, that is now reflected in the code Thanks to https://www.reddit.com/r/adventofcode/comments/r8n7fo/comment/hn6nh9c/

--- day4/day4p2.cpp
+++ day4/day4p2.cpp
@@ -32,8 +32,7 @@ void checkCombination(std::array<int, 25> bingoBoard, std::vector<int> winningNu
 
 bool hasWon(std::array<int, 25> bingoBoard, std::vector<int> winningNumbers) {
     std::vector<std::array<int, 5> > winningCombinations{{0,1,2,3,4},{5,6,7,8,9},{10,11,12,13,14},{15,16,17,18,19},{20,21,22,23,24}, /*rows*/\
-    {0,5,10,15,20},{1,6,11,16,21},{2,7,12,17,22},{3,8,13,18,23},{4,9,14,19,24},/*columns*/\
-    {0,6,12,18,24},{4,8,12,16,20}/*diagonals*/};
+    {0,5,10,15,20},{1,6,11,16,21},{2,7,12,17,22},{3,8,13,18,23},{4,9,14,19,24},/*columns*/};
     bool win = false;
     for (std::array<int, 5> c : winningCombinations) {
         checkCombination(bingoBoard, winningNumbers, c, win);
