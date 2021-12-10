8b9485435d9dd9a12219ba7deba203db890016b5
BenBean18 <beakerthespeaker@gmail.com>
2021-12-10 18:20:34 -0500

    Better visualization

--- day10/day10p2_visualization.cpp
+++ day10/day10p2_visualization.cpp
@@ -64,22 +64,54 @@ int main(int argc, char** argv) {
         }
         std::cout << std::endl;
     }
+    int counter = 0;
+    double timeMultiplier = 3;
     for (std::string s : strings) {
+        counter++;
         std::vector<int> calledOpeningIndices{};
         std::vector<char> calledOpeningCharacters{}; // This will store the opening characters we have encountered,
         // pushing to the back of the list. If we see a closing character, it must correspond to the last item in this vector.
         // If it does, the last item should be removed. If it doesn't, add to the score for the line.
         bool isIncomplete = true;
         for (int i = 0; i < s.size(); i++) {
+            std::cout << "\033[" << std::to_string(size.ws_row/2 - 1) << "H\033[48;5;237m"; // move cursor to middle - 1
+            std::cout << counter << "/" << strings.size();
+            std::cout << "\033[" << std::to_string(size.ws_row/2) << "H\033[48;5;237m"; // move cursor to middle
             char c = s[i];
             if (std::find(startingCharacters.begin(), startingCharacters.end(), c) != startingCharacters.end()) {
                 calledOpeningCharacters.push_back(c);
                 calledOpeningIndices.push_back(i);
             } else if (std::find(closingCharacters.begin(), closingCharacters.end(), c) != closingCharacters.end()) {
                 if (calledOpeningCharacters.size() == 0) {
+                    std::cout << "\r\033[48;5;238m";
+                    for (int i = 0; i < size.ws_col; i++) {
+                        std::cout << " ";
+                    }
+                    std::cout << "\r\033[48;5;238m";
+                    for (int i = 0; i < (size.ws_col - s.size())/2; i++) {
+                        std::cout << " ";
+                    }
+                    std::cout << s.substr(0, i);
+                    std::cout << "\033[48;5;197m" << s[i] << s.substr(i);
+                    std::cout << "\033[48;5;238m";
+                    std::flush(std::cout);
+                    std::this_thread::sleep_for(std::chrono::milliseconds((int)(timeMultiplier*500)));
                     isIncomplete = false;
                     break;
                 } else if (closingToOpening[c] != calledOpeningCharacters[calledOpeningCharacters.size()-1]) {
+                    std::cout << "\r\033[48;5;238m";
+                    for (int i = 0; i < size.ws_col; i++) {
+                        std::cout << " ";
+                    }
+                    std::cout << "\r\033[48;5;238m";
+                    for (int i = 0; i < (size.ws_col - s.size())/2; i++) {
+                        std::cout << " ";
+                    }
+                    std::cout << s.substr(0, i);
+                    std::cout << "\033[48;5;197m" << s[i] << s.substr(i);
+                    std::cout << "\033[48;5;238m";
+                    std::flush(std::cout);
+                    std::this_thread::sleep_for(std::chrono::milliseconds((int)(timeMultiplier*500)));
                     isIncomplete = false;
                     break;
                 } else {
@@ -89,37 +121,55 @@ int main(int argc, char** argv) {
             }
         }
         if (isIncomplete) {
+            // for (int)
             std::cout << "\033[48;5;238m";
             int color = (rand() % 170 + 50);
             std::cout << s;
             std::cout << "\033[38;5;" << color << "m";
             unsigned long score = 0;
             std::string chars;
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds((int)(timeMultiplier*100)));
+            std::vector<int> alreadyCompletedOpenCharacterIndices{};
             for (int i = calledOpeningCharacters.size() - 1; i >= 0; i--) {
+                ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
                 score *= 5ULL;
                 std::cout << "\r";
+                for (int i = 0; i < (size.ws_col - (chars.size()+s.size()))/2; i++) {
+                    std::cout << " ";
+                }
                 for (int j = 0; j < s.size(); j++) {
                     if (j == calledOpeningIndices[i]) {
                         std::cout << "\033[38;5;" << color << "m";
                         std::cout << s[j];
                         std::cout << "\033[0;m\033[48;5;238m";
+                        alreadyCompletedOpenCharacterIndices.push_back(j);
                         // highlight
+                    } else if (std::find(alreadyCompletedOpenCharacterIndices.begin(), alreadyCompletedOpenCharacterIndices.end(), j) != alreadyCompletedOpenCharacterIndices.end()) {
+                        std::cout << "\033[0;m\033[48;5;238m\033[38;5;226m";
+                        std::cout << s[j];
                     } else {
                         std::cout << "\033[0;m\033[48;5;238m";
                         std::cout << s[j];
                     }
                 }
+                std::cout << "\033[38;5;226m";
                 std::cout << chars;
                 std::cout << "\033[38;5;" << color << "m";
                 std::cout << openingToClosing[calledOpeningCharacters[i]];
                 chars += openingToClosing[calledOpeningCharacters[i]];
-                std::cout << "\033[0;m\033[48;5;238m";
+                std::cout << "\033[0;m\033[48;5;238m\r";
                 std::flush(std::cout);
-                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                std::this_thread::sleep_for(std::chrono::milliseconds((int)(timeMultiplier*50)));
                 score += scoreMap[openingToClosing[calledOpeningCharacters[i]]];
             }
-            std::cout << std::endl;
+            for (int i = 0; i < size.ws_col - (s.size() + chars.size()); i++) {
+                std::cout << " ";
+            }
+            std::cout << "\r";
+            for (int i = 0; i < size.ws_col; i++) {
+                std::cout << " ";
+            }
+            std::cout << "\r";
             std::cout << "\033[0;m";
             scores.push_back(score);
         }
