61b6a492d186ba657e50f80f18e7fccc45a1b508
BenBean18 <beakerthespeaker@gmail.com>
2021-12-12 14:45:52 -0500

    Day 12 Part 2 optimization with recursive multithreading...

--- day12/day12p2.cpp
+++ day12/day12p2.cpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <set>
 #include <regex>
+#include <thread>
+#include <mutex>
 
 std::vector<std::string> getStrings() {
     std::vector<std::string> strings;
@@ -65,35 +67,47 @@ static inline bool is_not_alphanum_lower(char c) {
     return (!isalnum(c) || !islower(c));
 }
 
+std::mutex mtx;
+std::vector<std::thread> threads;
 
 // currentPath is pair<path, hasVisitedASingleCaveTwiceYet>
 void computePath(std::pair<std::vector<std::string>,bool> currentPath, Graph<std::string> graph, std::vector<std::vector<std::string>> &paths) {
     if (currentPath.first[currentPath.first.size()-1] == "end") {
+        mtx.lock();
         paths.push_back(currentPath.first);
+        mtx.unlock();
         return;
     }
     for (std::string node : graph.neighbors(currentPath.first[currentPath.first.size()-1])) {
         // if it's already in the list, it can't be a small cave (lowercase) because that would cause it to be visited multiple times
         // except if no small cave has been visited twice yet
-        bool isSmallCave = (std::find_if(node.begin(), node.end(), is_not_alphanum_lower) == node.end()) && (node != "start") && (node != "end");
+        bool isSmallCave = node[0] >= 'a' && node[0] <= 'z';
         bool hasVisited = (std::find(currentPath.first.begin(), currentPath.first.end(), node) != currentPath.first.end());
         bool hasVisitedAndSmallCave = (hasVisited && isSmallCave);
         if (node == "end") {
             auto newPath = currentPath;
             newPath.first.push_back(node);
+            mtx.lock();
             paths.push_back(currentPath.first);
+            mtx.unlock();
         } else if (node == "start") {
             //
         } else if (!hasVisitedAndSmallCave) {
             auto newPath = currentPath;
             newPath.first.push_back(node);
-            computePath(newPath, graph, paths);
-        } else if (hasVisitedAndSmallCave) {
-            if ((std::count(currentPath.first.begin(), currentPath.first.end(), node) < 2) && (currentPath.second == false)) {
-                auto newPath = currentPath;
-                newPath.second = true; // this used to be currentPath.second = true, which was causing a bug...
-                newPath.first.push_back(node);
-                computePath(newPath, graph, paths);
+            if (currentPath.first.size() == 1) {
+                threads.emplace_back(computePath, newPath, graph, std::ref(paths));
+            } else {
+                computePath(newPath, graph, std::ref(paths));
+            }
+        } else if (hasVisitedAndSmallCave && (currentPath.second == false)) {
+            auto newPath = currentPath;
+            newPath.second = true; // this used to be currentPath.second = true, which was causing a bug...
+            newPath.first.push_back(node);
+            if (currentPath.first.size() == 1) {
+                threads.emplace_back(computePath, newPath, graph, std::ref(paths));
+            } else {
+                computePath(newPath, graph, std::ref(paths));
             }
         }
     }
@@ -109,6 +123,9 @@ int main(int argc, char** argv) {
             std::cout << s << (i < p.size() - 1 ? "," : "");
         }
         std::cout << std::endl;
+    }
+    for (auto &t : threads) {
+        t.join();
     }
     std::cout << "Paths: " << paths.size() << std::endl;
     return 0;
