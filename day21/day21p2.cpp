2cc6762da200af628bdcfa6bef877702081a3627
BenBean18 <beakerthespeaker@gmail.com>
2021-12-21 18:54:33 -0500

    In Progress: Day 21 Part 2

--- day21/day21p2.cpp
+++ day21/day21p2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <thread>
+#include <map>
 
 std::vector<std::string> getStrings() {
     std::vector<std::string> strings;
@@ -52,64 +52,73 @@ struct Game {
         p2Square = p2;
     }
 };
+inline bool operator<(Game a, Game b) {
+    return (a.p1Square*40 + a.p2Square*30 + a.p1Score*20 + a.p2Score*10) < (b.p1Square*40 + b.p2Square*30 + b.p1Score*20 + b.p2Score*10);
+}
 
 // can roll 3 (1/27),4 (3/27),5 (6/27),6 (7/27),7 (6/27),8 (3/27),9 (1/27) (7 total states)
 std::vector<int> rolls{3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 6, 5, 6, 7, 6, 7, 8, 5, 6, 7, 6, 7, 8, 7, 8, 9};
-std::vector<std::thread> threads;
 
-unsigned long long roll(Game game, unsigned long long &p1Count, unsigned long long &p2Count, unsigned long long recursionDepth = 0) {
-    for (int r1 = 1; r1 <= 3; r1++) {
-        Game game1 = game;
-        for (int r2 = 1; r2 <= 3; r2++) {
-            Game game2 = game1;
-            for (int r3 = 1; r3 <= 3; r3++) {
-                Game g = game2;
-                if (p1Turn) {
-                    g.p1Square += r1+r2+r3;
-                    g.p1Square %= 10;
-                    g.p1Score += g.p1Square + 1;
-                    p1Turn = false;
-                    if (g.p1Score >= 21) {
-                        p1Count += 1;
-                        g.p1Score = 0;
-                    } else {
-                        recursionDepth++;
-                        if (recursionDepth < 5) {
-                            threads.emplace_back(roll, g, std::ref(p1Count), std::ref(p2Count), recursionDepth);
-                        } else {
-                            roll(g, p1Count, p2Count, recursionDepth);
-                        }
-                    }
+using ull = unsigned long long;
+std::map<std::pair<Game, int>, int> memo; // value = int (winner), key=<Game (state before),int (roll)>
+std::map<std::pair<Game, int>, bool> inMemo; // key = memo key, value = in memo or not
+
+std::pair<ull, ull> roll(Game game, unsigned long long recursionDepth = 0) {
+    std::pair<ull, ull> sum{0,0};
+    for (int r : rolls) {
+        Game g = game;
+        if (!inMemo[{game,r}]) {
+            if (p1Turn) {
+                g.p1Square += r;
+                g.p1Square %= 10;
+                g.p1Score += g.p1Square + 1;
+                p1Turn = false;
+                if (g.p1Score >= 21) {
+                    g.p1Score = 0;
+                    sum.first++;
+                    memo[{game,r}] = 1;
+                    inMemo[{game,r}] = true;
                 } else {
-                    g.p2Square += r1+r2+r3;
-                    g.p2Square %= 10;
-                    g.p2Score += g.p2Square + 1;
-                    p1Turn = true;
-                    if (g.p2Score >= 21) {
-                        p2Count += 1;
-                        g.p2Score = 0;
-                    } else {
-                        recursionDepth++;
-                        if (recursionDepth < 5) {
-                            threads.emplace_back(roll, g, std::ref(p1Count), std::ref(p2Count), recursionDepth);
-                        } else {
-                            roll(g, p1Count, p2Count, recursionDepth);
-                        }
-                    }
+                    recursionDepth++;
+                    auto res = roll(g, recursionDepth);
+                    sum.first += res.first;
+                    sum.second += res.second;
                 }
+            } else {
+                g.p2Square += r;
+                g.p2Square %= 10;
+                g.p2Score += g.p2Square + 1;
+                p1Turn = true;
+                if (g.p2Score >= 21) {
+                    g.p2Score = 0;
+                    sum.second++;
+                    memo[{game,r}] = 1;
+                    inMemo[{game,r}] = true;
+                } else {
+                    recursionDepth++;
+                    auto res = roll(g, recursionDepth);
+                    sum.first += res.first;
+                    sum.second += res.second;
+                }
+            }
+        } else {
+            auto res = memo[{game,r}];
+            if (res == 1) {
+                sum.first++;
+            } else {
+                sum.second++;
             }
         }
     }
-    return std::max(p1Count, p2Count);
+    return sum;
 }
 
 int main(int argc, char** argv) {
     std::vector<std::string> strings = getStrings();
     int player1Start = strings[0][strings[0].size()-1] - '0';
     int player2Start = strings[1][strings[1].size()-1] - '0';
-    unsigned long long p1c;
-    unsigned long long p2c;
     bool player1Won = false;
-    std::cout << roll(Game(player1Start, player2Start), p1c, p2c) << std::endl;
+    auto res = roll(Game(player1Start, player2Start));
+    std::cout << res.first << " " << res.second << std::endl;
     return 0;
 }
