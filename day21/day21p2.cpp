1d3360df5cbdada64785f8de3d751c5f47c2c689
BenBean18 <beakerthespeaker@gmail.com>
2021-12-21 22:02:53 -0500

    In Progress: Day 21 Part 2

--- day21/day21p2.cpp
+++ day21/day21p2.cpp
@@ -39,7 +39,6 @@ std::vector<int> getInts() {
     return ints;
 }
 
-bool p1Turn = true;
 int rollCount = 0;
 
 struct Game {
@@ -47,6 +46,7 @@ struct Game {
     int p2Square;
     int p1Score = 0;
     int p2Score = 0;
+    bool p1Turn = true;
     Game(int p1, int p2) {
         p1Square = p1;
         p2Square = p2;
@@ -63,24 +63,24 @@ using ull = unsigned long long;
 std::map<std::pair<Game, int>, int> memo; // value = int (winner), key=<Game (state before),int (roll)>
 std::map<std::pair<Game, int>, bool> inMemo; // key = memo key, value = in memo or not
 
-std::pair<ull, ull> roll(Game game, unsigned long long recursionDepth = 0) {
+std::pair<ull, ull> roll(Game game, int winningScore = 21, unsigned long long recursionDepth = 0) {
     std::pair<ull, ull> sum{0,0};
     for (int r : rolls) {
-        Game g = game;
+        std::cout << recursionDepth << ":" << r << " ";
+        Game g((const Game)game);
         if (!inMemo[{game,r}]) {
-            if (p1Turn) {
+            if (g.p1Turn) {
                 g.p1Square += r;
                 g.p1Square %= 10;
                 g.p1Score += g.p1Square + 1;
-                p1Turn = false;
-                if (g.p1Score >= 21) {
+                g.p1Turn = false;
+                if (g.p1Score >= winningScore) {
                     g.p1Score = 0;
                     sum.first++;
                     memo[{game,r}] = 1;
                     inMemo[{game,r}] = true;
                 } else {
-                    recursionDepth++;
-                    auto res = roll(g, recursionDepth);
+                    auto res = roll(g, winningScore, recursionDepth+1);
                     sum.first += res.first;
                     sum.second += res.second;
                 }
@@ -88,15 +88,14 @@ std::pair<ull, ull> roll(Game game, unsigned long long recursionDepth = 0) {
                 g.p2Square += r;
                 g.p2Square %= 10;
                 g.p2Score += g.p2Square + 1;
-                p1Turn = true;
-                if (g.p2Score >= 21) {
+                g.p1Turn = true;
+                if (g.p2Score >= winningScore) {
                     g.p2Score = 0;
                     sum.second++;
-                    memo[{game,r}] = 1;
+                    memo[{game,r}] = 2;
                     inMemo[{game,r}] = true;
                 } else {
-                    recursionDepth++;
-                    auto res = roll(g, recursionDepth);
+                    auto res = roll(g, winningScore, recursionDepth+1);
                     sum.first += res.first;
                     sum.second += res.second;
                 }
@@ -113,12 +112,19 @@ std::pair<ull, ull> roll(Game game, unsigned long long recursionDepth = 0) {
     return sum;
 }
 
+// Try the lanternfish approach (std::map<Game,unsigned long long> where value = how many Games corresponding to that value)
+// Store whose turn it is in the game within Game
+// While nonzero values exist:
+//     Compute next game (or add to win counts and remove) for each Game
+//     See day6p2.cpp
+
 int main(int argc, char** argv) {
     std::vector<std::string> strings = getStrings();
     int player1Start = strings[0][strings[0].size()-1] - '0';
     int player2Start = strings[1][strings[1].size()-1] - '0';
+    std::cout << player1Start << " and " << player2Start << std::endl;
     bool player1Won = false;
-    auto res = roll(Game(player1Start, player2Start));
+    auto res = roll(Game(player1Start, player2Start), 2);
     std::cout << res.first << " " << res.second << std::endl;
     return 0;
 }
