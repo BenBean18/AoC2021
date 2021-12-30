28826bc6b0922ff7527efaa2fe13d12b972bf3a6
BenBean18 <beakerthespeaker@gmail.com>
2021-12-30 12:35:23 -0500

    Yay again :) And fancy terminal stuff

--- day25/day25.cpp
+++ day25/day25.cpp
@@ -3,8 +3,7 @@
 #include <vector>
 #include <chrono> // for sleeping
 #include <thread>
-
-// #define VISUALIZE
+#include <string.h>
 
 void delay(int ms) {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
@@ -102,19 +101,32 @@ void step(std::vector<std::string> &data) {
 }
 
 int main(int argc, char** argv) {
+    bool visualize = false;
+    if (argc >= 2) {
+        if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--visualize")) {
+            visualize = true;
+        }
+        if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-?") || !strcmp(argv[1], "--help")) {
+            std::cout << "Help:" << std::endl;
+            std::cout << "\t-v, --visualize: visualize sea cucumbers moving" << std::endl;
+            std::cout << "\t-h, -?, --help: show this help page" << std::endl;
+            std::cout << "\nHave a nice day!" << std::endl;
+            return 0;
+        }
+    }
     std::vector<std::string> current = getStrings();
     std::vector<std::string> previous{};
     unsigned long s = 0;
     while (previous != current) {
-#ifdef VISUALIZE
-        std::cout << "\033[;H"; // clear screen
-        std::cout << "Step " << s << std::endl;
-        std::cout << std::endl;
-        for (std::string line : current) {
-            std::cout << line << std::endl;
+        if (visualize) {
+            std::cout << "\033[;H"; // clear screen
+            std::cout << "Step " << s << std::endl;
+            std::cout << std::endl;
+            for (std::string line : current) {
+                std::cout << line << std::endl;
+            }
+            delay(100);
         }
-        delay(100);
-#endif
         previous = current;
         step(current);
         s++;
